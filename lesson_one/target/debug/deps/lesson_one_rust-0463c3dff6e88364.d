abd04502fe8b8bdb05b1f495cf9138a684c09a7a
CharlesKibet <charlesbiegon973@gmail.com>
2026-06-19 21:21:23 +0300

    chore: move to lesson one folder

---
+++ lesson_one/target/debug/deps/lesson_one_rust-0463c3dff6e88364.d
@@ -0,0 +1,5 @@
+/home/charles/Desktop/funprojects/lesson_one_rust/target/debug/deps/lesson_one_rust-0463c3dff6e88364.d: src/main.rs
+
+/home/charles/Desktop/funprojects/lesson_one_rust/target/debug/deps/liblesson_one_rust-0463c3dff6e88364.rmeta: src/main.rs
+
+src/main.rs:
---
+++ lesson_one/target/debug/lesson_one_rust.d
@@ -0,0 +1,1 @@
+/home/charles/Desktop/funprojects/lesson_one_rust/target/debug/lesson_one_rust: /home/charles/Desktop/funprojects/lesson_one_rust/src/main.rs
