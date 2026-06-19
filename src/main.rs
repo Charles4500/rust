cd4365d4dc18612f14422500ca31a67249de29cd
CharlesKibet <charlesbiegon973@gmail.com>
2026-06-19 19:37:28 +0300

    chore: getting started

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+    //Ownership --> Every values in rust has a variable that owns it
+    
+    //Borrowing --> Enables parts of code to access data without transfer of ownership. This basically is to allow for efficient memory management and facilitates concurrency
+    
+}
