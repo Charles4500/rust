f24877171391864ea7db148f8275522e94de04f5
CharlesKibet <charlesbiegon973@gmail.com>
2026-06-22 12:01:44 +0300

    challenge2: print staircase

--- dsa/src/main.rs
+++ dsa/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
     /*
     input: 1 + 2
     output: 3
@@ -8,10 +8,25 @@ fn main() {
     1<= a, b <=1000
     */
 
-    let result = solve_me_first(1, 2);
-    println!("{}", result);
+    // let result = solve_me_first(1, 2);
+    // println!("{}", result);
+
+    //Challenge 2 --> Staircase
+    print_stair_case(6);
 }
 
-fn solve_me_first(a: i32, b: i32) -> i32 {
-    return a + b;
+// fn solve_me_first(a: i32, b: i32) -> i32 {
+//     return a + b;
+// }
+
+fn print_stair_case(n: i32) {
+    for num in 1..=n {
+        for _ in 0..(n - num) {
+            print!(" ")
+        }
+        for _ in 0..num {
+            print!("#")
+        }
+        println!()
+    }
 }
